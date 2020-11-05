33735f9a8f51d322a8cc928e67d3b7ba0a400542
seredonato <54689539+seredonato@users.noreply.github.com>
2020-11-05 18:35:44 -0300

    modificacion del maquetado con bootstrap

--- footer.php
+++ footer.php
@@ -1,26 +1,59 @@
-<footer class="w3-center w3-black w3-padding-16">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1640.6884989672753!2d-58.56398904764693!3d-34.67043346619271!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc62cc3ef7083%3A0x8867107f425fade5!2sUniversidad%20Nacional%20de%20La%20Matanza!5e0!3m2!1ses-419!2sar!4v1604349244297!5m2!1ses-419!2sar"
-            width="350" height="250" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
-            tabindex="0" id="mapa"></iframe>
+<footer class="page-footer font-weight-bold mt-lg-5 p-3">
+    <h4 class="text-white text-center blockquote mb-2">CÓMO LLEGAR</h4>
+        <div class="d-flex justify-content-around">
+            <div class="d-flex flex-column mt-2 text-white text-center">
+                <div class="d-flex flex-row">
+                    <i class="fas fa-id-badge"></i><p class="pb-2 ml-2">Coloca, Fiorella</p>
+                </div>
+                <div class="d-flex flex-row">
+                    <i class="fas fa-id-badge"></i><p class="pb-2 ml-2">Donato, Serena Rocío</p>
+                </div>
+                <div class="d-flex flex-row">
+                    <i class="fas fa-id-badge"></i><p class="pb-2 ml-2">Lopez Ducas, Abril</p>
+                </div>
+                <div class="d-flex flex-row">
+                    <i class="fas fa-id-badge"></i><p class="pb-2 ml-2">Reynoso, Franco Joaquín</p>
+                </div>
+            </div>
+            <div class="text-white d-flex flex-column mt-4 text-center">
+                <div class="d-flex flex-row">
+                    <i class="fas fa-map-marker-alt"></i><p class="pb-2 ml-2">Florencio Varela 1903, B1754</p>
+                </div>
+                <div class="d-flex flex-row">
+                    <i class="fas fa-map-marker-alt"></i><p class="pb-2 ml-2">San Justo, Buenos Aires, Argentina</p>
+                </div>
+                <div class="d-flex flex-row">
+                    <i class="fas fa-phone-alt"></i><p class="pb-2 ml-2">+54 9 11 2345-6789</p>
+                </div>
+            </div>
+            <div class="mt-4">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3281.3738521385076!2d-58.565059249360196!3d-34.67051288034624!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcc62cc3ef7083%3A0x8867107f425fade5!2sUniversidad%20Nacional%20de%20La%20Matanza!5e0!3m2!1ses-419!2sar!4v1604603845805!5m2!1ses-419!2sar"
+                    width="400" height="150" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false"
+                    tabindex="0"></iframe>
+            </div>
+        </div>
+
 </footer>
 
+<!-- Optional JavaScript; choose one of the two! -->
 <script>
-    var slideIndex = 0;
-    carousel();
-
-    function carousel() {
-        var i;
-        var x = document.getElementsByClassName("mySlides");
-        for (i = 0; i < x.length; i++) {
-            x[i].style.display = "none";
-        }
-        slideIndex++;
-        if (slideIndex > x.length) {
-            slideIndex = 1
-        }
-        x[slideIndex - 1].style.display = "block";
-        setTimeout(carousel, 10000); // Change image every 2 seconds
-    }
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').trigger('focus')
+    })
 </script>
+
+<!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx"
+        crossorigin="anonymous"></script>
+
+<!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+-->
 </body>
 </html>
--- registro.php
+++ registro.php
@@ -1,24 +1,50 @@
-
 <?php
-include_once ("header.php");
+include_once("header.php");
 ?>
 
-    <div class="w3-container w3-padding-32 " id="contact">
-        <form action="/action_page.php" target="_blank" class="w3-third w3-display-middle  w3-card-2 w3-padding">
-            <h3 class="w3-border-bottom w3-border-light-grey w3-padding-16">Registrarse</h3>
-            <input class="w3-input w3-border" type="text" placeholder="Ingrese su nombre y apellido" required name="nombre">
-            <input class="w3-input w3-section w3-border" type="text" placeholder="Ingrese su email" required name="email">
-            <input class="w3-input w3-section w3-border" type="number" placeholder="Ingrese su documento" required name="dni">
-            <input class="w3-input w3-section w3-border" type="password" placeholder="Ingrese su contraseña" required name="clave">
-            <input class="w3-input w3-section w3-border" type="password" placeholder="Repita su contraseña" required name="clave">
-            <label><p>Ingrese fecha de nacimiento</p></label>
-            <input type="date" id="birthday" name="birthday" required>
-            <button class="w3-button w3-black w3-section w3-block" type="submit">
-                    <i class="fa fa-paper-plane"></i> Enviar
-                </button>
-        </form>
-    </div>
-    <!-- End page content -->
+<div class="container justify-content-center" id="contenedor-registro">
+    <h3 class="mt-3 text-center display-4">Registro</h3>
+    <hr>
+    <form action="#" enctype="multipart/form-data" method="post" class="mt-3">
+        <div class="form-row mt-4 mb-3">
+            <div class="col">
+                <label for="nombre">Nombre:</label>
+                <input type="text" class="form-control" placeholder="Ingrese su Nombre" required name="email">
+            </div>
+            <div class="col">
+                <label for="dni">DNI:</label>
+                <input type="number" class="form-control" placeholder="Ingrese su DNI" required name="dni">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="email" class="form-control" placeholder="Ingrese su Email" required name="email">
+        </div>
+
+        <div class="form-group">
+            <label for="password">Contraseña:</label>
+            <input type="password" class="form-control" placeholder="Ingrese su Contraseña" required name="password">
+        </div>
+
+        <div class="form-group">
+            <label for="password">Confirme Contraseña:</label>
+            <input type="password" class="form-control" placeholder="Repita su Contraseña" required name="password">
+        </div>
+
+        <div class="form-group">
+            <label for="genero">Ingrese su Fecha de Nacimiento:</label>
+            <input type="date" class="form-control" id=birthday required name=birthday>
+        </div>
+
+        <div class="mt-4 mb-4 text-center">
+            <button type="submit" class="btn btn-dark btn-lg btn-block">Registrarse</button>
+        </div>
+    </form>
+
+</div>
+
+<!-- End page content -->
 <?php
- /* include_once ("footer.php.php");  PROBLEMAS TECNICOS CON EL DISPLAY DE ARRIBA , JDFKSD*/
+    include_once ("footer.php");
 ?>
