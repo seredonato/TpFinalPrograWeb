fb82282dc6b0c0091c24c256359abc70fd68f5df
fioreColoca <fiorecoloca99@gmail.com>
2020-11-10 19:51:57 -0300

    isset en linea 278

--- controller/RegistroController.php
+++ controller/RegistroController.php
@@ -1,6 +1,6 @@
 <?php
 
-class RegistroController{
+class RegistroController {
     private $render;
 
     public function __construct($render){
@@ -9,6 +9,19 @@ class RegistroController{
 
     public function execute(){
         echo $this->render->render("view/registroView.php");
+    }
+
+    public function procesarFormulario(){
+        $nombre = $_POST["nombre"];
+        $dni= $_POST["dni"];
+        $email["email"]  = $_POST["email"];
+        $password["password"]  = $_POST["password"];
+        $birthday["birthday"]  = $_POST["birthday"];
+
+        $usuario = $this->model->crearUsuario($nombre,$dni,$email,$password,$birthday);
+        $data["usuario"] = $usuario[0];
+
+        echo $this->renderer->render( "view/registroView.php", $data);
     }
 
 }
