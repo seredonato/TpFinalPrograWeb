0009edc0aeedfc2ae304f27998c98eb3f0949d59
fioreColoca <fiorecoloca99@gmail.com>
2020-11-16 12:43:17 -0300

    agregue choferes/adm y eso de los camiones

--- public/grupo11.sql
+++ public/grupo11.sql
@@ -1,3 +1,5 @@
+drop database grupo11;
+
 CREATE DATABASE grupo11;
 USE grupo11;
 
@@ -13,5 +15,51 @@ CREATE TABLE usuario (
   tipo varchar(100),
   PRIMARY KEY (id)
 );
+
+CREATE TABLE chofer(
+tipo_licencia varchar(100) not null,
+licencia varchar(100) not null,
+numero_usuario int not null,
+primary key (numero_usuario),
+foreign key (numero_usuario) references usuario(id));
+
+CREATE TABLE administrador (
+numero_usuario int not null,
+primary key (numero_usuario),
+foreign key (numero_usuario) references usuario(id));
+
+CREATE TABLE mecanico (
+numero_usuario int not null,
+primary key (numero_usuario),
+foreign key (numero_usuario) references usuario(id));
+
+CREATE TABLE supervisor (
+numero_usuario int not null,
+primary key (numero_usuario),
+foreign key (numero_usuario) references usuario(id));
+
+CREATE TABLE equipo(
+año_fabricacion date not null,
+estado boolean not null,
+patente varchar(40) not null,
+nro_chasis int not null,
+id int AUTO_INCREMENT NOT NULL,
+primary key (id));
+
+CREATE TABLE TRACTOR(
+marca varchar(100) not null,
+modelo varchar(100) not null,
+calendario_service date,
+nro_motor int not null,
+kilometraje int not null,
+id int not null,
+primary key (id),
+foreign key (id) references equipo(id));
+
+CREATE TABLE ACOPLADO (
+tipo_acoplado varchar(100),
+id int not null,
+primary key (id),
+foreign key (id) references equipo(id));
 
 select * from usuario;
