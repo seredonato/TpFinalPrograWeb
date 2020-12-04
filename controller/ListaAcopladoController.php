e661d54740db2813f0c523d5c7c9d15b776417f3
fioreColoca <fiorecoloca99@gmail.com>
2020-12-03 23:02:15 -0300

    cambios en acoplado para seisones

--- controller/ListaAcopladoController.php
+++ controller/ListaAcopladoController.php
@@ -17,53 +17,122 @@ class ListaAcopladoController
     }
 
     public function registroAcoplado(){
-        $acoplado = $_POST["acoplado"];
-        $patente = $_POST["patente"];
-        $chasis = $_POST["chasis"];
         $data["login"] = $this->loginModel->ifSesionIniciada();
-        $data["acoplados"] = $this->acopladoModel->mostrarAcoplado();
 
-        $result = $this->acopladoModel->registrarAcoplado( $acoplado,$patente,$chasis);
-        if ($result === "Ingrese contenido en el campo requerido"){
-            $data["registroAcopladoError"] = $result;
-            echo $this->render->render("view/listaAcopladosView.php", $data);
+        if ($data["login"]) {
+            $rol = $this->loginModel->getRolDeUsuario($_SESSION["nombreUsuario"]);
+
+            $valorDelRol = $this->loginModel->confirmarRolUsuario($rol);
+
+            $valorAdmin = $this->loginModel->confirmarAdmin($valorDelRol);
+            $valorChofer = $this->loginModel->confirmarChofer($valorDelRol);
+            $valorMecanico = $this->loginModel->confirmarMecanico($valorDelRol);
+            $valorSupervisor = $this->loginModel->confirmarSupervisor($valorDelRol);
+
+            $data["valorAdmin"] = $valorAdmin;
+            $data["valorChofer"] = $valorChofer;
+            $data["valorMecanico"] = $valorMecanico;
+            $data["valorSupervisor"] = $valorSupervisor;
+
+            $acoplado = $_POST["acoplado"];
+            $patente = $_POST["patente"];
+            $chasis = $_POST["chasis"];
+            $data["login"] = $this->loginModel->ifSesionIniciada();
+            $data["acoplados"] = $this->acopladoModel->mostrarAcoplado();
+
+            $result = $this->acopladoModel->registrarAcoplado( $acoplado,$patente,$chasis);
+            if ($result === "Ingrese contenido en el campo requerido"){
+                $data["registroAcopladoError"] = $result;
+                echo $this->render->render("view/listaAcopladosView.php", $data);
+            }
+            $data["acoplados"] = $this->acopladoModel->mostrarAcoplado();
+            echo $this->render->render("view/listaAcopladosView.php",$data);
+
+        } else{
+            echo $this->render->render("view/inicio.php", $data);
         }
-        $data["login"] = $this->loginModel->ifSesionIniciada();
-        $data["acoplados"] = $this->acopladoModel->mostrarAcoplado();
-        echo $this->render->render("view/listaAcopladosView.php",$data);
-
     }
 
 
     public function execute()
     {
         $data["login"] = $this->loginModel->ifSesionIniciada();
-        $data["acoplados"] = $this->acopladoModel->mostrarAcoplado();
-        echo $this->render->render("view/listaAcopladosView.php", $data);
+        if ($data["login"]) {
+            $rol = $this->loginModel->getRolDeUsuario($_SESSION["nombreUsuario"]);
+
+            $valorDelRol = $this->loginModel->confirmarRolUsuario($rol);
+
+            $valorAdmin = $this->loginModel->confirmarAdmin($valorDelRol);
+            $valorChofer = $this->loginModel->confirmarChofer($valorDelRol);
+            $valorMecanico = $this->loginModel->confirmarMecanico($valorDelRol);
+            $valorSupervisor = $this->loginModel->confirmarSupervisor($valorDelRol);
+
+            $data["valorAdmin"] = $valorAdmin;
+            $data["valorChofer"] = $valorChofer;
+            $data["valorMecanico"] = $valorMecanico;
+            $data["valorSupervisor"] = $valorSupervisor;
+            $data["acoplados"] = $this->acopladoModel->mostrarAcoplado();
+            echo $this->render->render("view/listaAcopladosView.php", $data);
+
+        } else{
+            echo $this->render->render("view/inicio.php", $data);
+        }
     }
 
     public function modificarAcoplado(){
-        $tipo = $_POST["tipo"];
-        $id = $_POST["id"];
-        $patente = $_POST["patente"];
-        $chasis = $_POST["chasis"];
-
-        $result = $this->acopladoModel->modificarAcoplado($id,$tipo,$patente,$chasis);
-
         $data["login"] = $this->loginModel->ifSesionIniciada();
-        $data["acoplados"] = $this->acopladoModel->mostrarAcoplado();
+        if ($data["login"]) {
+            $rol = $this->loginModel->getRolDeUsuario($_SESSION["nombreUsuario"]);
 
-        echo $this->render->render("view/listaAcopladosView.php",$data);
+            $valorDelRol = $this->loginModel->confirmarRolUsuario($rol);
+
+            $valorAdmin = $this->loginModel->confirmarAdmin($valorDelRol);
+            $valorChofer = $this->loginModel->confirmarChofer($valorDelRol);
+            $valorMecanico = $this->loginModel->confirmarMecanico($valorDelRol);
+            $valorSupervisor = $this->loginModel->confirmarSupervisor($valorDelRol);
+
+            $data["valorAdmin"] = $valorAdmin;
+            $data["valorChofer"] = $valorChofer;
+            $data["valorMecanico"] = $valorMecanico;
+            $data["valorSupervisor"] = $valorSupervisor;
+            $tipo = $_POST["tipo"];
+            $id = $_POST["id"];
+            $patente = $_POST["patente"];
+            $chasis = $_POST["chasis"];
+
+            $result = $this->acopladoModel->modificarAcoplado($id,$tipo,$patente,$chasis);
+            $data["acoplados"] = $this->acopladoModel->mostrarAcoplado();
+            echo $this->render->render("view/listaAcopladosView.php", $data);
+        } else{
+            echo $this->render->render("view/inicio.php", $data);
+        }
     }
 
     public function eliminarAcoplado(){
-
-        $id = $_GET["id"];
-
-        $this->acopladoModel->eliminarAcoplado($id);
         $data["login"] = $this->loginModel->ifSesionIniciada();
-        $data["acoplados"] = $this->acopladoModel->mostrarAcoplado();
+        if ($data["login"]) {
+            $rol = $this->loginModel->getRolDeUsuario($_SESSION["nombreUsuario"]);
 
-        echo $this->render->render("view/listaAcopladosView.php", $data);
+            $valorDelRol = $this->loginModel->confirmarRolUsuario($rol);
+
+            $valorAdmin = $this->loginModel->confirmarAdmin($valorDelRol);
+            $valorChofer = $this->loginModel->confirmarChofer($valorDelRol);
+            $valorMecanico = $this->loginModel->confirmarMecanico($valorDelRol);
+            $valorSupervisor = $this->loginModel->confirmarSupervisor($valorDelRol);
+
+            $data["valorAdmin"] = $valorAdmin;
+            $data["valorChofer"] = $valorChofer;
+            $data["valorMecanico"] = $valorMecanico;
+            $data["valorSupervisor"] = $valorSupervisor;
+            $id = $_GET["id"];
+            $this->acopladoModel->eliminarAcoplado($id);
+            $data["login"] = $this->loginModel->ifSesionIniciada();
+
+            $data["acoplados"] = $this->acopladoModel->mostrarAcoplado();
+            echo $this->render->render("view/listaAcopladosView.php", $data);
+
+        } else{
+            echo $this->render->render("view/inicio.php", $data);
+        }
     }
 }
