da6bb8ce2ba1ea455f8b890e67d54e87130dd800
fioreColoca <fiorecoloca99@gmail.com>
2020-12-08 20:24:09 -0300

    cambios en el diseño, ahora es un naranjita mas oscuro y botones azules

--- view/listaCalendario.php
+++ view/listaCalendario.php
@@ -6,14 +6,14 @@
     <hr>
     <div class="d-flex p-2 justify-content-center">
         {{#valorMecanico}}
-        <button type="button" class="btn btn-dark mr-2 ml-2" data-toggle="modal" data-target="#modalRegistrarCalendario">
+        <button type="button" class="btn text-white" style="background: #1E0C80" data-toggle="modal" data-target="#modalRegistrarCalendario">
             <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-plus" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                 <path fill-rule="evenodd" d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z"/>
             </svg> Agregar fecha </button>
         {{/valorMecanico}}
-        <a href="/ListaEquipo" class="btn btn-dark"> Ver equipos</a>
-        <a href="/ListaAcoplado" class="btn btn-dark ml-2"> Ver acoplado</a>
-        <a href="/ListaTractor" class="btn btn-dark ml-2"> Ver a tractores</a>
+        <a href="/ListaEquipo" class="btn text-white ml-2" style="background: #1E0C80"> Ver equipos</a>
+        <a href="/ListaAcoplado" class="btn text-white ml-2" style="background: #1E0C80"> Ver acoplado</a>
+        <a href="/ListaTractor" class="btn text-white ml-2" style="background: #1E0C80"> Ver a tractores</a>
     </div>
 
 
--- view/listaPedidosView.php
+++ view/listaPedidosView.php
@@ -4,10 +4,10 @@
     <div class="container mt-2 listaUsuarios text-center">
         <h2 class="titulosindex">Todos los Pedidos</h2>
         <hr>
-        <a href="/listaPedidos/pedidosSinProforma" type="button" class="btn btn-dark">Pedidos Sin Proforma</a>
-        <a href="/listaPedidos/pedidosPendientes" type="button" class="btn btn-dark">Pedidos Pendientes</a>
-        <a href="/listaPedidos/pedidosActivos" type="button" class="btn btn-dark">Pedidos Activos</a>
-        <a href="/listaPedidos/pedidosFinalizados" type="button" class="btn btn-dark">Pedidos Finalizados</a>
+        <a href="/listaPedidos/pedidosSinProforma" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Sin Proforma</a>
+        <a href="/listaPedidos/pedidosPendientes" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Pendientes</a>
+        <a href="/listaPedidos/pedidosActivos" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Activos</a>
+        <a href="/listaPedidos/pedidosFinalizados" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Finalizados</a>
         <div class="row my-5">
             {{#pedidos}}
             {{>informacionPedidos}}
