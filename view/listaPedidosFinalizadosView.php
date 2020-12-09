29f30bde653e6369ac90f1feb0968b3bf0cd4539
fioreColoca <fiorecoloca99@gmail.com>
2020-12-09 19:09:57 -0300

    agregue botones en pedidos para navegar mejor

--- view/listaPedidosFinalizadosView.php
+++ view/listaPedidosFinalizadosView.php
@@ -4,6 +4,10 @@
     <div class="container mt-2 listaUsuarios text-center">
         <h2 class="titulosindex">Pedidos Finalizados</h2>
         <hr>
+        <a href="/listaPedidos/pedidosSinProforma" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Sin Proforma</a>
+        <a href="/listaPedidos/pedidosPendientes" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Pendientes</a>
+        <a href="/listaPedidos/pedidosActivos" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Activos</a>
+        <a href="/listaPedidos/listaPedidosView" type="button" class="btn text-white" style="background: #1E0C80">Todos los pedidos</a>
         <div class="row my-5">
             {{#pedidosFinalizados}}
             {{>informacionPedidosConProforma}}
