0256e1354046211c0b5dd66a6133f3ffe90f29a3
fioreColoca <fiorecoloca99@gmail.com>
2020-12-09 16:37:48 -0300

    agregue los botones para ver en todas las vistas de viaje

--- view/viajesActivosView.php
+++ view/viajesActivosView.php
@@ -4,6 +4,9 @@
     <div class="container mt-2 listaUsuarios text-center">
         <h2 class="titulosindex">Viajes Activos</h2>
         <hr>
+        <a href="/viajes/viajesPendientes" type="button" class="btn text-white" style="background: #1E0C80">Viajes Pendientes</a>
+        <a href="/viajes/viajesFinalizados" type="button" class="btn text-white" style="background: #1E0C80">Viajes Finalizados</a>
+        <a href="/viajes/viajes" type="button" class="btn text-white" style="background: #1E0C80">Ver todos los viajes</a>
         <div class="row my-5">
             {{#viajesActivos}}
             {{>informacionViajesActivos}}
--- view/viajesPendientesView.php
+++ view/viajesPendientesView.php
@@ -4,6 +4,9 @@
     <div class="container mt-2 listaUsuarios text-center">
         <h2 class="titulosindex">Viajes Pendientes</h2>
         <hr>
+        <a href="/viajes/viajesActivos" type="button" class="btn text-white" style="background: #1E0C80">Viajes Activos</a>
+        <a href="/viajes/viajesFinalizados" type="button" class="btn text-white" style="background: #1E0C80">Viajes Finalizados</a>
+        <a href="/viajes/viajes" type="button" class="btn text-white" style="background: #1E0C80">Ver todos los viajes</a>
         <div class="row my-5">
             {{#viajesPendientes}}
             {{>informacionViajesPendientes}}
