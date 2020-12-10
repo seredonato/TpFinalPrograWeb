887dd93faabdc470997a17c9106063987ed97985
fioreColoca <fiorecoloca99@gmail.com>
2020-12-09 22:48:52 -0300

    separe los botones en formato celular vista de chofer para q no se junten

--- view/viajesFinalizadosView.php
+++ view/viajesFinalizadosView.php
@@ -4,9 +4,9 @@
     <div class="container mt-2 listaUsuarios text-center">
         <h2 class="titulosindex">Viajes Finalizados</h2>
         <hr>
-        <a href="/viajes/viajesActivos" type="button" class="btn text-white" style="background: #1E0C80">Viajes Activos</a>
-        <a href="/viajes/viajesPendientes" type="button" class="btn text-white" style="background: #1E0C80">Viajes Pendientes</a>
-        <a href="/viajes/viajes" type="button" class="btn text-white" style="background: #1E0C80">Ver todos los viajes</a>
+        <a href="/viajes/viajesActivos" type="button" class="btn text-white mt-1" style="background: #1E0C80">Viajes Activos</a>
+        <a href="/viajes/viajesPendientes" type="button" class="btn text-white mt-1" style="background: #1E0C80">Viajes Pendientes</a>
+        <a href="/viajes/viajes" type="button" class="btn text-white mt-1" style="background: #1E0C80">Ver todos los viajes</a>
         <div class="row my-5">
             {{#viajesFinalizados}}
             {{>informacionViajes}}
