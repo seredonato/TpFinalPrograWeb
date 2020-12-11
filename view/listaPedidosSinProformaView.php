37a394fca44ed86b3fc4fbb75a5a77e1020b6f7d
fioreColoca <fiorecoloca99@gmail.com>
2020-12-11 00:03:01 -0300

    arregle una cagada q hice

--- view/listaPedidosSinProformaView.php
+++ view/listaPedidosSinProformaView.php
@@ -4,10 +4,12 @@
     <div class="container mt-2 listaUsuarios">
         <h2 class="titulosindex text-center">Pedidos Sin Proforma Hecha</h2>
         <hr>
+        <div  class="text-center">
         <a href="/listaPedidos/listaPedidosView" type="button" class="btn text-white" style="background: #1E0C80">Todos los pedidos</a>
         <a href="/listaPedidos/pedidosPendientes" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Pendientes</a>
         <a href="/listaPedidos/pedidosActivos" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Activos</a>
         <a href="/listaPedidos/pedidosFinalizados" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Finalizados</a>
+        </div>
         <div class="row my-5">
             {{#pedidosNoProforma}}
             {{>informacionPedidos}}
--- view/listaPedidosView.php
+++ view/listaPedidosView.php
@@ -4,11 +4,13 @@
     <div class="container mt-2 listaUsuarios">
         <h2 class="titulosindex text-center">Todos los Pedidos</h2>
         <hr>
+        <div class="text-center">
         <a href="/listaPedidos/pedidosSinProforma" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Sin Proforma</a>
         <a href="/listaPedidos/pedidosPendientes" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Pendientes</a>
         <a href="/listaPedidos/pedidosActivos" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Activos</a>
         <a href="/listaPedidos/pedidosFinalizados" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Finalizados</a>
-        <div class="row my-5">
+        </div>
+         <div class="row my-5">
             {{#pedidos}}
             {{>informacionPedidos}}
             {{/pedidos}}
