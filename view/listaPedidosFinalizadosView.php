7aaef43d791de14214864840a61a23109454cf36
fioreColoca <fiorecoloca99@gmail.com>
2020-12-11 14:13:12 -0300

    margenes en los botones de navegacion para q no se choquen y nuevo grafico

--- view/listaPedidosFinalizadosView.php
+++ view/listaPedidosFinalizadosView.php
@@ -4,10 +4,10 @@
     <div class="container mt-2 listaUsuarios text-center">
         <h2 class="titulosindex">Pedidos Finalizados</h2>
         <hr>
-        <a href="/listaPedidos/pedidosSinProforma" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Sin Proforma</a>
-        <a href="/listaPedidos/pedidosPendientes" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Pendientes</a>
-        <a href="/listaPedidos/pedidosActivos" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Activos</a>
-        <a href="/listaPedidos/listaPedidosView" type="button" class="btn text-white" style="background: #1E0C80">Todos los pedidos</a>
+        <a href="/listaPedidos/pedidosSinProforma" type="button" class="btn text-white mt-2" style="background: #1E0C80">Pedidos Sin Proforma</a>
+        <a href="/listaPedidos/pedidosPendientes" type="button" class="btn text-white mt-2" style="background: #1E0C80">Pedidos Pendientes</a>
+        <a href="/listaPedidos/pedidosActivos" type="button" class="btn text-white mt-2" style="background: #1E0C80">Pedidos Activos</a>
+        <a href="/listaPedidos/listaPedidosView" type="button" class="btn text-white mt-2" style="background: #1E0C80">Todos los pedidos</a>
         <div class="row my-5">
             {{#pedidosFinalizados}}
             {{>informacionPedidosFinalizadosEstadistica}}
--- view/listaPedidosPendientesView.php
+++ view/listaPedidosPendientesView.php
@@ -4,10 +4,10 @@
     <div class="container mt-2 listaUsuarios text-center">
         <h2 class="titulosindex">Pedidos Pendientes</h2>
         <hr>
-        <a href="/listaPedidos/pedidosSinProforma" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Sin Proforma</a>
-        <a href="/listaPedidos/listaPedidosView" type="button" class="btn text-white" style="background: #1E0C80">Todos los pedidos</a>
-        <a href="/listaPedidos/pedidosActivos" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Activos</a>
-        <a href="/listaPedidos/pedidosFinalizados" type="button" class="btn text-white" style="background: #1E0C80">Pedidos Finalizados</a>
+        <a href="/listaPedidos/pedidosSinProforma" type="button" class="btn text-white mt-2" style="background: #1E0C80">Pedidos Sin Proforma</a>
+        <a href="/listaPedidos/listaPedidosView" type="button" class="btn text-white mt-2" style="background: #1E0C80">Todos los pedidos</a>
+        <a href="/listaPedidos/pedidosActivos" type="button" class="btn text-white mt-2" style="background: #1E0C80">Pedidos Activos</a>
+        <a href="/listaPedidos/pedidosFinalizados" type="button" class="btn text-white mt-2" style="background: #1E0C80">Pedidos Finalizados</a>
         <div class="row my-5">
             {{#pedidosPendientes}}
             {{>informacionPedidosConProforma}}
